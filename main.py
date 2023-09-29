289846d02e88abf5bc365b8e79e57850d055b793
Abhishek Choudhury <abhishek@Abhisheks-MacBook-Pro.local>
2023-09-29 12:41:32 -0700

    add '/' path response

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ recipe_schema = RecipeSchema(many=True)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return "Welcome to your Recipe Book", 200
 
 
 @main.route('/profile', methods=['GET'])
